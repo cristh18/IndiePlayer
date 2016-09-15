dd08b2fdde40d719861bf13bd40b24e35fea1d97
cristhian_tolosa <cristhian.tolosa@grability.com>
2016-09-15 10:59:16 -0500

    Add IndieApplication to manifest

--- app/src/main/java/com/cristto/indieplayer/api/services/HttpConstants.java
+++ app/src/main/java/com/cristto/indieplayer/api/services/HttpConstants.java
@@ -1,9 +1,12 @@
-package com.cristto.indieplayer.api.Constants;
+package com.cristto.indieplayer.api.services;
 
 import com.cristto.indieplayer.app.IndiePlayerApplication;
 
-public class HttpConstants {
+public abstract class HttpConstants {
 
     public static final String TRACKS_SERVICE_URL = IndiePlayerApplication.getTracksUrl();
     public static final String CLIENT_ID = IndiePlayerApplication.getClientId();
+
+    protected abstract String getTrackServiceUrl();
+
 }
